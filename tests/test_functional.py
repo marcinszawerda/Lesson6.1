bcefab7c034724f1dfb05c9721ffe2ffac69afa1
marcinszawerda <marcin.szawerda@student.put.poznan.pl>
2026-05-12 23:01:29 +0200

    Usprawnienie test_total_due_plan

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -8,7 +8,12 @@ from src.models import Bill
 def test_total_due_pln():
     parameters = Parameters()
     manager = Manager(parameters)
-    rozliczenie = manager.get_settlement("apart-polanka", 2025, 1)
-    print(rozliczenie)
-    spr = manager.create_tenants_settlements(rozliczenie)
     
+    rozliczenie = manager.get_settlement("apart-polanka", 2025, 1)
+    
+    assert rozliczenie is not None, "Brak danych rozliczeniowych dla podanego miesiąca i mieszkania"
+    
+    spr = manager.create_tenants_settlements(rozliczenie)
+    suma_najemcow = sum(lokator.total_due_pln for lokator in spr)
+    
+    assert suma_najemcow == rozliczenie.total_due_pln
